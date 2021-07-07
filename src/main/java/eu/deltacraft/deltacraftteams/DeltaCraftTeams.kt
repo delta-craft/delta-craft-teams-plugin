ff90d0e5a02ac33b5590500f487c3b325b6a6e71
Fjarik <jirkafalta@gmail.com>
2021-07-07 18:03:08 +0200

    Basic objects

--- src/main/java/eu/deltacraft/deltacraftteams/DeltaCraftTeams.kt
+++ src/main/java/eu/deltacraft/deltacraftteams/DeltaCraftTeams.kt
@@ -3,6 +3,7 @@ package eu.deltacraft.deltacraftteams
 import eu.deltacraft.deltacraftteams.commands.MainCommand
 import eu.deltacraft.deltacraftteams.interfaces.IDbConnector
 import eu.deltacraft.deltacraftteams.listeners.PlayerBlockListener
+import eu.deltacraft.deltacraftteams.listeners.PlayerJoinListener
 import eu.deltacraft.deltacraftteams.managers.DeltaCraftTeamsManager
 import eu.deltacraft.deltacraftteams.types.DbConnector
 import eu.deltacraft.deltacraftteams.utils.enums.Settings
@@ -70,6 +71,7 @@ class DeltaCraftTeams : JavaPlugin() {
         val pluginManager = this.server.pluginManager
 
         pluginManager.registerEvents(PlayerBlockListener(this), this)
+        pluginManager.registerEvents(PlayerJoinListener(this, dbConnector), this)
         this.debugMsg("PlayerBlockListener loaded")
     }
 
--- src/main/java/eu/deltacraft/deltacraftteams/listeners/PlayerJoinListener.kt
+++ src/main/java/eu/deltacraft/deltacraftteams/listeners/PlayerJoinListener.kt
@@ -1,17 +1,28 @@
 package eu.deltacraft.deltacraftteams.listeners
 
 import eu.deltacraft.deltacraftteams.DeltaCraftTeams
+import eu.deltacraft.deltacraftteams.interfaces.IDbConnector
+import eu.deltacraft.deltacraftteams.managers.UserConnectionManager
+import net.kyori.adventure.text.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
-class PlayerJoinListener(private val plugin: DeltaCraftTeams) : Listener {
+class PlayerJoinListener(private val plugin: DeltaCraftTeams, dbConnector: IDbConnector) : Listener {
+    private val mgr = UserConnectionManager(dbConnector)
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
 
-        // TODO: Check player in database
+        val res = mgr.get(player)
+        if (res == null) {
+            // TODO: Better component
+            player.kick(Component.text("You are not registred"))
+            return
+        }
+
+        player.sendMessage(res.toString())
 
     }
 
