a562b23ef6a67a64716b900b46e906eb1af68cbf
Fjarik <jirkafalta@gmail.com>
2021-07-10 15:08:19 +0200

    Removed DB settings

--- src/main/java/eu/deltacraft/deltacraftteams/types/ConnectionString.kt
+++
@@ -1,3 +0,0 @@
-package eu.deltacraft.deltacraftteams.types
-
-class ConnectionString(val host: String, val login: String, val password: String)
--- src/main/java/eu/deltacraft/deltacraftteams/utils/enums/Settings.kt
+++ src/main/java/eu/deltacraft/deltacraftteams/utils/enums/Settings.kt
@@ -17,30 +17,6 @@ enum class Settings(
         "Sentry DSN",
         false,
         "STRING"
-    ),
-    DATABASESERVER(
-        "system.database.server",
-        "Database server",
-        false,
-        "STRING"
-    ),
-    DATABASENAME(
-        "system.database.name",
-        "Database name",
-        false,
-        "STRING"
-    ),
-    DATABASELOGIN(
-        "system.database.username",
-        "Database server login name ",
-        false,
-        "STRING"
-    ),
-    DATABASEPASSWORD(
-        "system.database.password",
-        "Database server password",
-        false,
-        "STRING"
     );
     /*,
     SPECTATEMAXDISTANCE(
