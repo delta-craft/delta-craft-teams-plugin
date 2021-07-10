b41e6a65dfdd88812dd0dbeecc140d6e592e6931
Kubas556 <jakubsedlak102@gmail.com>
2021-07-10 18:25:18 +0200

    response deserialization

--- src/main/java/eu/deltacraft/deltacraftteams/listeners/PlayerJoinAttemptListener.kt
+++ src/main/java/eu/deltacraft/deltacraftteams/listeners/PlayerJoinAttemptListener.kt
@@ -1,38 +1,41 @@
 package eu.deltacraft.deltacraftteams.listeners
 
 import eu.deltacraft.deltacraftteams.DeltaCraftTeams
+import net.kyori.adventure.text.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
 import java.util.StringJoiner
 import java.util.HashMap
-import com.sun.xml.internal.ws.streaming.XMLStreamWriterUtil.getOutputStream
-import com.sun.xml.internal.ws.streaming.XMLStreamWriterUtil.getOutputStream
 import org.json.JSONObject
+import org.omg.CORBA.Object
+import java.awt.TextComponent
 import java.io.InputStreamReader
 import java.io.BufferedReader
 import java.util.stream.Collectors
 
+enum class ValidateError constructor(val value: String) {
+    ArgumentsError("arguments_error"),
+    MethodNotValid("method_not_valid"),
+    MissingConsent("missing_consent"),
+    MissingName ("missing_name"),
+    UuidNotValid("uuid_not_valid"),
+    NotRegistered("not_registered"),
+    Unauthorized("unauthorized"),
+    Unknown("unknown");
 
-
-
-
-
-
-
-
-
+    companion object {
+        fun from(findValue: String): ValidateError = ValidateError.values().first { it.value == findValue }
+    }
+}
 
 class PlayerJoinAttemptListener(private val plugin: DeltaCraftTeams) : Listener {
 
     @EventHandler
     fun onPlayerAttemptJoin(playerJoinEvent:AsyncPlayerPreLoginEvent) {
-        //URL("https://portal.deltacraft.eu/api/validate?name=Kubas445&uuid=4b715c2f-fd4d-32c9-bcb6-a70dceb93928").readText();
-
         val arguments = HashMap<String, String>()
         arguments["name"] = playerJoinEvent.name
         arguments["uuid"] = playerJoinEvent.uniqueId.toString()
@@ -40,7 +43,7 @@ class PlayerJoinAttemptListener(private val plugin: DeltaCraftTeams) : Listener 
         for ((key, value) in arguments)
             sj.add(URLEncoder.encode(key, "UTF-8") + "="
                     + URLEncoder.encode(value, "UTF-8"))
-        val out = sj.toString()//.getBytes(StandardCharsets.UTF_8)
+        val out = sj.toString()
 
         val url = URL("https://portal.deltacraft.eu/api/validate?"+out);
         val con = url.openConnection();
@@ -62,8 +65,25 @@ class PlayerJoinAttemptListener(private val plugin: DeltaCraftTeams) : Listener 
         val responseBody = br.lines().collect(Collectors.joining())
         val json = JSONObject(responseBody);
         if(!json.getBoolean("success")) {
-            plugin.logger.info("Player: " + playerJoinEvent.name + " tried to join, but he does not accepted the rules.")
-            playerJoinEvent.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER,"You have to accept the rules first!")
+            val errorsString = (json.getJSONArray("errors")).map { x -> x.toString() }
+
+            val errors = errorsString.map { x -> ValidateError.from(x) }
+            //val errors = emptyList<ValidateError>().toMutableList()
+            /*for (e in errorsString) {
+                if(ValidateError.values().any { it.name == e }) {
+                    errors.add(ValidateError.values().first { it.name == e })
+                }
+            }*/
+
+            for (e in errors) {
+                when(e) {
+                    ValidateError.NotRegistered -> {
+                        plugin.logger.info("Player: ${playerJoinEvent.name} tried to join, but he does not accepted the rules.")
+                        playerJoinEvent.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, Component.text(""))
+                        return
+                    }
+                }
+            }
         }
     }
 }
