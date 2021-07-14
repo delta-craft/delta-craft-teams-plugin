60465306c49a97714ab84b0d7dfd3bf6ae8aba7d
Fjarik <jirkafalta@gmail.com>
2021-07-14 21:30:48 +0200

    Validation response bugfix

--- src/main/java/eu/deltacraft/deltacraftteams/types/ConnectionResponse.kt
+++ src/main/java/eu/deltacraft/deltacraftteams/types/ConnectionResponse.kt
@@ -2,7 +2,7 @@ package eu.deltacraft.deltacraftteams.types
 
 import eu.deltacraft.deltacraftteams.utils.enums.ValidateError
 
-data class ConnectionResponse(val sucess: Boolean, val error: String) {
+data class ConnectionResponse(val content: Boolean, val error: String) {
 
     fun getErrorEnum(): ValidateError? {
         return ValidateError.from(error)
