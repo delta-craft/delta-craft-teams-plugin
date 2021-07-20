fe72fc3be548d04fdad2393594cf4c29e817fad7
Fjarik <jirkafalta@gmail.com>
2021-07-20 22:55:17 +0200

    Quick fix

--- src/main/java/eu/deltacraft/deltacraftteams/types/CheckChatResult.kt
+++ src/main/java/eu/deltacraft/deltacraftteams/types/CheckChatResult.kt
@@ -1,4 +1,6 @@
 package eu.deltacraft.deltacraftteams.types
 
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class CheckChatResult(val content: Boolean, val message: String? = null)
