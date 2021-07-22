a6fba1e7aea7d17377a8deabf61dee2b481bb5fb
Fjarik <jirkafalta@gmail.com>
2021-07-22 18:27:31 +0200

    MajorTeam color

--- src/main/java/eu/deltacraft/deltacraftteams/utils/enums/MajorTeam.kt
+++ src/main/java/eu/deltacraft/deltacraftteams/utils/enums/MajorTeam.kt
@@ -1,14 +1,20 @@
 package eu.deltacraft.deltacraftteams.utils.enums
 
-enum class MajorTeam(val value: String) {
-    Blue("blue"),
-    Red("red"),
-    None("none");
+import net.kyori.adventure.text.format.NamedTextColor
+
+enum class MajorTeam(val value: String, val color: NamedTextColor) {
+    Blue("blue", NamedTextColor.BLUE),
+    Red("red", NamedTextColor.RED),
+    None("none", NamedTextColor.GRAY);
 
     companion object {
         fun from(findValue: String): MajorTeam? = values().firstOrNull { it.value == findValue }
 
-        fun from(findValue: String, default: MajorTeam): MajorTeam =
-            values().firstOrNull { it.value == findValue } ?: default
+        fun from(findValue: String?, default: MajorTeam): MajorTeam {
+            if (findValue == null) {
+                return default
+            }
+            return values().firstOrNull { it.value == findValue } ?: default
+        }
     }
 }
